1975e65a89b77c8d16739d6aca0d23916b157c1d
Michael Cordani <cordani1@live.com>
2023-05-06 06:20:34 +0000

    ruby book chapter 2

---
+++ variables/example_2.rb
@@ -0,0 +1,3 @@
+total = 0
+[1, 2, 3].each { |number| total += number }
+puts total
---
+++ variables/name.rb
@@ -0,0 +1,7 @@
+puts "Type your first name:"
+first_name = gets.chomp
+puts "Type your last name:"
+last_name = gets.chomp
+puts "Hello, " + first_name + " " + last_name + "!"
+
+
